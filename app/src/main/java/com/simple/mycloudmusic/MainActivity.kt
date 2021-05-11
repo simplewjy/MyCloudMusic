220e81c2c0562a6600f28c30e5e6eaa2e6199983
wujunyu <wujunyu@dycjr.com>
2021-05-11 18:18:05 +0800

    feat: 预加载页全屏

--- app/src/main/java/com/simple/mycloudmusic/MainActivity.kt
+++ app/src/main/java/com/simple/mycloudmusic/MainActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import com.simple.mycloudmusic.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val activityMainBinding =
+//            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             wb_test.webViewClient = object : WebViewClient() {
                 override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
--- app/src/main/java/com/simple/mycloudmusic/module/splash/SplashActivity.kt
+++ app/src/main/java/com/simple/mycloudmusic/module/splash/SplashActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
 import com.simple.mycloudmusic.MainActivity
 import com.simple.mycloudmusic.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
