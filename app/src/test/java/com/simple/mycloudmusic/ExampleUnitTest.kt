9c0b9c378dfec4334634edceb1abeb22857ee6a9
simple.wu <wujunyu@aidong.me>
2023-01-04 15:47:43 +0800

    feat: 新增一些算法

--- app/src/test/java/com/simple/mycloudmusic/ExampleUnitTest.kt
+++ app/src/test/java/com/simple/mycloudmusic/ExampleUnitTest.kt
@@ -1,11 +1,9 @@
 package com.simple.mycloudmusic
 
-import android.util.Log
-import org.junit.Test
-
 import org.junit.Assert.*
-import kotlin.math.max
-import kotlin.math.min
+import org.junit.Test
+import java.util.*
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -211,8 +209,8 @@ class ExampleUnitTest {
 
     @Test
     fun test15() {
-        val aaa = arrayOf("flower", "flow", "flight")
-        val longestCommonPrefix = longestCommonPrefix(aaa)
+        val aaa = intArrayOf(1, 2, -2, -1)
+        val longestCommonPrefix = threeSum(aaa)
         println("test15: --------------$longestCommonPrefix")
     }
 
@@ -221,10 +219,328 @@ class ExampleUnitTest {
      * 你返回所有和为 0 且不重复的三元组。注意：答案中不可以包含重复的三元组。
      */
     fun threeSum(nums: IntArray): List<List<Int>> {
-        val resultList = arrayListOf<List<Int>>()
-//        resultList.toList()
+        val result = mutableListOf<List<Int>>()
+        if (nums.size < 3) {
+            return result
+        }
+        //先从小到大排序
+        nums.sort()
+        for (i in 0 until nums.size - 2) {
+            //如果该数大于0，则从后面再找2个数，3个数的和必然大于0
+            if (nums[i] > 0) {
+                break
+            }
+            if (i > 0 && nums[i - 1] == nums[i]) continue
+            var j = i + 1
+            var k = nums.size - 1
+            while (j < k) {
+                val sum = nums[i] + nums[j] + nums[k]
+                when {
+                    sum > 0 -> {
+                        k--
+                    }
+                    sum == 0 -> {
+                        result.add(listOf(nums[i], nums[j], nums[k]))
+                        while (j < k && nums[j] == nums[++j]) {
+                        }
+                        while (j < k && nums[k] == nums[--k]) {
+                        }
+                    }
+                    else -> {
+                        j++
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+    @Test
+    fun test16() {
+        val aaa = intArrayOf(-1, 2, 1, -4)
+        val longestCommonPrefix = threeSumClosest(aaa, 1)
+        println("test16: --------------$longestCommonPrefix")
+    }
+
+    /**
+     * 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+     * 返回这三个数的和。
+     */
+    fun threeSumClosest(nums: IntArray, target: Int): Int {
+        var result = Int.MAX_VALUE
+        if (nums.size < 3) return 0
+        if (nums.size == 3) return nums.sum()
+        //将数组从小到大排序
+        nums.sort()
+        for (i in 0 until nums.size - 2) {
+            //去重处理
+            if (i > 0 && nums[i] == nums[i - 1]) continue
+            var j = i + 1
+            var k = nums.size - 1
+            while (j < k) {
+                val tempValue = nums[i] + nums[j] + nums[k]
+                val differenceValue = tempValue - target
+                when {
+                    differenceValue == 0 -> {
+                        return tempValue
+                    }
+                    differenceValue < 0 -> {
+                        if (Math.abs(differenceValue) < Math.abs(result - target)) {
+                            result = tempValue
+                        }
+                        j++
+                    }
+                    else -> {
+                        if (Math.abs(differenceValue) < Math.abs(result - target)) {
+                            result = tempValue
+                        }
+                        k--
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+    @Test
+    fun test17() {
+        val aaa = "23"
+        val result = letterCombinations(aaa)
+        println("test17: --------------$result")
+    }
+
+    private val digistMap = HashMap<Char, CharArray>()
+
+    fun letterCombinations(digits: String): List<String> {
+        //声明map对应数字与字母
+        digistMap['2'] = charArrayOf('a', 'b', 'c')
+        digistMap['3'] = charArrayOf('d', 'e', 'f')
+        digistMap['4'] = charArrayOf('g', 'h', 'i')
+        digistMap['5'] = charArrayOf('j', 'k', 'l')
+        digistMap['6'] = charArrayOf('m', 'n', 'o')
+        digistMap['7'] = charArrayOf('p', 'q', 'r', 's')
+        digistMap['8'] = charArrayOf('t', 'u', 'v')
+        digistMap['9'] = charArrayOf('w', 'x', 'y', 'z')
+
+        val resultList = arrayListOf<String>()
+        if (digits.isEmpty()) return resultList
+
+        val builder = StringBuilder()
+
+        fun backtrack(index: Int) {
+            // 当索引与数组长度相同时，跳出递归
+            if (index == digits.length) {
+                resultList.add(builder.toString())
+                return
+            }
+            for (c in digistMap[digits[index]]!!) {
+                builder.append(c)
+                // 递归
+                backtrack(index + 1)
+                // 回溯
+                builder.deleteLastChar()
+            }
+        }
+        backtrack(0)
         return resultList
     }
+
+    private fun StringBuilder.deleteLastChar() {
+        if (this.isNotEmpty()) {
+            this.deleteCharAt(this.length - 1)
+        }
+    }
+
+    @Test
+    fun test18() {
+        val aaa = intArrayOf(1000000000, 1000000000, 1000000000, 1000000000)
+        val longestCommonPrefix = fourSum(aaa, -294967296)
+        println("test18: --------------$longestCommonPrefix")
+    }
+
+    /**
+     *  给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]]
+     * （若两个四元组元素一一对应，则认为两个四元组重复）：
+     *  0 <= a, b, c, d < n
+     *  a、b、c 和 d 互不相同
+     *  nums[a] + nums[b] + nums[c] + nums[d] == target
+     *  你可以按 任意顺序 返回答案 。
+     */
+    fun fourSum(nums: IntArray, target: Int): List<List<Int>> {
+        val resultList = arrayListOf<List<Int>>()
+        if (nums.size < 4) return resultList
+        nums.sort()
+        //四个指针i,j,k,l
+        for (i in 0 until nums.size - 3) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue
+            for (j in i + 1 until nums.size - 2) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue
+                var k = j + 1
+                var l = nums.size - 1
+
+                while (k < l) {
+                    val sum = nums[i].toLong() + nums[j] + nums[k] + nums[l]
+                    when {
+                        sum > target -> {
+                            while (k < l && nums[l] == nums[--l]) {
+                            }
+                        }
+                        sum < target -> {
+                            while (k < l && nums[k] == nums[++k]) {
+                            }
+                        }
+                        else -> {
+                            resultList.add(listOf(nums[i], nums[j], nums[k], nums[l]))
+                            while (k < l && nums[k] == nums[++k]) {
+                            }
+                            while (k < l && nums[l] == nums[--l]) {
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return resultList
+    }
+
+
+    @Test
+    fun test19() {
+        var li = ListNode(0)
+        var li1 = ListNode(1)
+        var li2 = ListNode(2)
+        var li3 = ListNode(3)
+        var li4 = ListNode(4)
+        li.next = li1
+        li1.next = li2
+        li2.next = li3
+        li3.next = li4
+        var test = removeNthFromEnd(li, 2)
+        val sb = StringBuilder()
+        while (test != null) {
+            sb.append("${test.`val`},")
+            test = test.next
+        }
+        println("test19: --------------$sb")
+    }
+
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        val pre = ListNode(0).apply {
+            this.next = head
+        }
+        var fastNode: ListNode? = pre
+        var slowNode: ListNode? = pre
+        for (i in 0..n) {
+            fastNode = fastNode?.next
+        }
+        while (fastNode != null) {
+            slowNode = slowNode?.next
+            fastNode = fastNode.next
+        }
+        slowNode?.next = slowNode?.next?.next
+        return pre.next
+    }
+
+    @Test
+    fun test20() {
+        val test = isValid("()")
+        println("test20: --------------$test")
+    }
+
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     * 有效字符串需满足：
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 每个右括号都有一个对应的相同类型的左括号。
+     */
+    fun isValid(s: String): Boolean {
+        val stack = LinkedList<Char>()
+        for (char in s) {
+            when {
+                char == '(' -> {
+                    stack.push(')')
+                }
+                char == '{' -> {
+                    stack.push('}')
+                }
+                char == '[' -> {
+                    stack.push(']')
+                }
+                stack.isEmpty() || stack.pop() != char -> {
+                    return false
+                }
+            }
+        }
+        return stack.isEmpty()
+    }
+
+    @Test
+    fun test21() {
+        var li = ListNode(0)
+        var li1 = ListNode(1)
+        var li2 = ListNode(2)
+        var li3 = ListNode(3)
+        var li4 = ListNode(4)
+        li.next = li1
+        li1.next = li2
+        li2.next = li3
+        li3.next = li4
+
+
+        var li5 = ListNode(5)
+        var li6 = ListNode(6)
+        var li7 = ListNode(7)
+        var li8 = ListNode(8)
+        var li9 = ListNode(9)
+        li5.next = li6
+        li6.next = li7
+        li7.next = li8
+        li8.next = li9
+        val sb = StringBuilder()
+        var test = mergeTwoLists(li, li5)
+        while (test != null) {
+            sb.append("${test.`val`},")
+            test = test.next
+        }
+
+        println("test21: --------------$sb")
+    }
+
+    /**
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     */
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        return when {
+            list1 == null -> list2
+            list2 == null -> list1
+            list1.`val` < list2.`val` -> {
+                list1.next = mergeTwoLists(list1.next, list2)
+                list1
+            }
+            else -> {
+                list2.next = mergeTwoLists(list1, list2.next)
+                list2
+            }
+        }
+    }
+
+    @Test
+    fun test855() {
+        val nums1 = intArrayOf(1, 1, 3, 2)
+        val nums2 = intArrayOf(2, 3)
+        val nums3 = intArrayOf(3)
+        val resultList = twoOutOfThree(nums1, nums2, nums3)
+        println("test855: --------------$resultList")
+    }
+
+//    fun seat(): Int {
+//
+//    }
+//
+//    fun leave(p: Int) {
+//
+//    }
 
     @Test
     fun test2032() {
---
+++ app/src/test/java/com/simple/mycloudmusic/ListNode.kt
@@ -0,0 +1,10 @@
+package com.simple.mycloudmusic
+
+/**
+ * @description
+ * @author simple.wu
+ * @date 2023/1/4
+ */
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
